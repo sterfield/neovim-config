77f38aba177ac53f3598ddfa9cd2a38d006084eb
Guillaume Loetscher <guillaume.loetscher@datadoghq.com>
2024-05-03 23:32:28 +0200

    adding keymap for telescope + telescope project

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,10 +1,21 @@
 return {
     {
-        'nvim-telescope/telescope.nvim',
-        tag = '0.1.2',
-        dependencies = { 'nvim-lua/plenary.nvim' },
-        config = function()
-            defaults = {}
-        end,
-    }
+        "nvim-telescope/telescope.nvim",
+        branch = "0.1.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope-project.nvim",
+        },
+        keys = {
+            { "<Leader>ff", "<cmd>Telescope find_files<CR>", desc = "Telescope (files)" },
+            { "<Leader>fg", "<cmd>Telescope live_grep<CR>",  desc = "Telescope (grep search" },
+            { "<Leader>fb", "<cmd>Telescope buffers<CR>",    desc = "Telescope (buffers)" },
+            { "<Leader>fh", "<cmd>Telescope help_tags<CR>",  desc = "Telescope (help tags)" },
+            {
+                "<Leader>fp",
+                "<cmd>lua require'telescope'.extensions.project.project{}<CR>",
+                desc = "Telescope (project)",
+            },
+        },
+    },
 }
